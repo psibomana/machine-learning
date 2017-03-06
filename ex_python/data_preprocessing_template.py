af0bc25e6b2489f0de5c6dfd82dfdabe204a2e33
SOPAMI <sopami@ubuntu.ubuntu-domain>
2017-03-06 20:54:31 +0300

    Week 3 exercices

---
+++ ex_python/data_preprocessing_template.py
@@ -0,0 +1,10 @@
+# Import packages
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[: , :-1].values
+y = dataset.iloc[: , 3].values
+
+print(y);
