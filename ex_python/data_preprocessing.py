34455321893219590bb7d5212e836fe36bb51744
SOPAMI <sopami@ubuntu.ubuntu-domain>
2017-03-06 22:44:40 +0300

    Data preprocessing template - Python

--- ex_python/data_preprocessing.py
+++ ex_python/data_preprocessing.py
@@ -13,6 +13,28 @@ imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+# Categorical encoding data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+oneHotEncode = OneHotEncoder(categorical_features = [0])
+X = oneHotEncode.fit_transform(X).toarray()
+
+labelEncoder_Y = LabelEncoder()
+y = labelEncoder_Y.fit_transform(y)
+
 # Splitting the dataset into the training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+print(X_train)
+print(X_test)
